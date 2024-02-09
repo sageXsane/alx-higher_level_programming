8c3350bc5660fdf9411eb29417f12834c68ff7f5
sageXsane <sagexsane@LAPTOP-ALBHI1UR>
2024-02-09 19:49:42 +0200

    Fix print format task + Do tasks 4 and 5

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,4 +2,7 @@
 
 if __name__ == "__main__":
     import hidden_4
-    dir(hidden_4)
+    nameslist = dir(hidden_4)
+    for name in nameslist:
+        if name[0] != '_':
+            print("{}".format(name))
