c39b1dc9a7a949b8f943473fcd627387ef311a73
sageXsane <sagexsane@LAPTOP-ALBHI1UR>
2024-02-10 17:45:55 +0200

    Add case list = None in task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -6,5 +6,6 @@ def print_reversed_list_integer(my_list=[]):
     Args:
         my_list = integer list
     """
-    for i in range(len(my_list) - 1, -1, -1):
-        print("{:d}".format(my_list[i]))
+    if my_list != None:
+        for i in range(len(my_list) - 1, -1, -1):
+            print("{:d}".format(my_list[i]))
