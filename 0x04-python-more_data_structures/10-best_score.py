97080ac6c0f63b37be7af501b63905168179c7cb
sageXsane <sanelisiwebhengu01@gmail.com>
2024-02-11 19:54:18 +0200

    Add tasks 8 - 11

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if a_dictionary is not None:
+        score = 0
+        best = ""
+        for key, value in a_dictionary.items():
+            if value > score:
+                score = value
+                best = key
+        return best
+    else:
+        return None
