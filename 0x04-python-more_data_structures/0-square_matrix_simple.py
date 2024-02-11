b8c55c291986fb1d57aa23f42db06f0379dd3c23
sageXsane <sanelisiwebhengu01@gmail.com>
2024-02-11 10:02:45 +0200

    Fix pycodestyle error in task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -5,6 +5,6 @@ def square_matrix_simple(matrix=[]):
     sq_matrix = []
 
     for i in range(0, len(matrix)):
-        sq_numbers = list(map(lambda x : x*x, matrix[i]))
+        sq_numbers = list(map(lambda x: x*x, matrix[i]))
         sq_matrix.append(sq_numbers)
     return sq_matrix
