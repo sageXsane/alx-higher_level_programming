cb89254d16a563ef44d6bc82bce8d4fa3790f613
sageXsane <sanelisiwebhengu01@gmail.com>
2024-02-14 09:25:05 +0200

    Add tasks 1-6

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/python3
+
+def safe_print_division(a, b):
+    try:
+        ans = a/b
+    except ZeroDivisionError:
+        ans = None
+    finally:
+        print("Inside result: {}".format(ans))
+    return ans
