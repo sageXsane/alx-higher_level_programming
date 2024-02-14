5d91856f1175bbfbee0cb88a09d04d6db3514c72
sageXsane <sanelisiwebhengu01@gmail.com>
2024-02-14 09:43:09 +0200

    Fix errors task 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,8 +2,7 @@
 
 def safe_print_integer(value):
     try:
-        if isinstance(value, int):
-            print("{:d}".format(value))
-            return True
-    except ValueError:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
         return False
