a17f684c2c505c3f895dca6b427ababa7a6882f9
sageXsane <sanelisiwebhengu01@gmail.com>
2024-02-16 08:34:57 +0200

    Add tasks 0-2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+class Square:
+
+    def __init__(self, size=0):
+        if size is not None:
+            try:
+                if not isinstance(size, int):
+                    raise TypeError("size must be an integer")
+                elif size < 0:
+                    raise ValueError("size must be >= 0")
+                else:
+                    self.__size = size
+            except TypeError as e:
+                print(e)
+            except ValueError as e:
+                print(e)
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+This directory contains learning tasks relating to classes in Python
