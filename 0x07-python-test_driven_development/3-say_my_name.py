c2902288c4d3862b6b4d9580dee112135d0db705
sageXsane <sanelisiwebhengu01@gmail.com>
2024-02-19 23:37:29 +0200

    Add task 3

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" say my name module
+    Has one function called say_my_name -
+    displays the given name and surname
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """ say_my_name function:
+
+        prints the name and surname given in a related message
+
+        Args:
+            first_name: string value representing the name of a person
+            last_name: string representing the surname of a person (optional)
+
+        Expections:
+            TypeError: first_name must be a string or
+                    last_name must be a string
+    """
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
+
+    if __name__ == "__main__":
+        import doctest
+        doctest.testmod(Verbose=True)
