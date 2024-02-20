45d91e1949c340c869e14fcbda26e5e8da97726f
sageXsane <sanelisiwebhengu01@gmail.com>
2024-02-20 11:44:20 +0200

    Add task 4

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" print square module
+    Has a function print_square that prints size X size square
+"""
+
+
+def print_square(size=0):
+    """ print_square function
+
+        prints a square depending on the dimensions of size
+
+        Args:
+            size: size of the square
+
+        Exceptions:
+            TypeError: size must be an integer
+            ValueError: size must be >= 0
+    """
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if size == 0:
+        print(end="\n")
+    for row in range(0, size):
+        for col in range(0, size):
+            print("#", end="")
+        print(end="\n")
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(Verbose=True)
