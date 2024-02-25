95ac8cb415d1440ef942b4c947bf730bbb434d43
sageXsane <sanelisiwebhengu01@gmail.com>
2024-02-25 02:33:55 +0200

    Add newlines to all tasks

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -20,8 +20,8 @@ class Rectangle:
 
         """
 
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
