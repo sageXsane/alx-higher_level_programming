f70d825d8f1fb842947c24c06cdb5fc9e7b7d042
sageXsane <sanelisiwebhengu01@gmail.com>
2024-04-15 23:37:29 +0200

    Add task 101

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" add_attribute module"""
+
+
+def add_attribute(a_class, atrribute, value):
+    """ add_attribute method:
+        adds a new attribute to an object if it’s possible
+        (only to custom classes and not builtin ones)
+
+        Args:
+            a_class: class name
+            attribute: atrribute
+            value: attribute value
+    """
+    if isinstance(a_class, (int, float, str, list, dict, tuple, set)):
+        raise TypeError("can't add new attribute")
+    else:
+        setattr(a_class, atrribute, value)
