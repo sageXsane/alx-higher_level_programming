e1fffadf76e30d37fe9b7f9f08f1691bff51f01d
sageXsane <sanelisiwebhengu01@gmail.com>
2024-04-16 00:12:31 +0200

    Fix task 101(4)

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -12,11 +12,11 @@ def add_attribute(a_class, attribute, value):
             attribute: atrribute
             value: attribute value
     """
-    if isinstance(a_class, (int, float, str, list, dict, tuple, set)):
+    if isinstance(a_class, (int, float, str, tuple)):
         raise TypeError("can't add new attribute")
     elif '__slots__' in dir(a_class):
         raise TypeError("can't add new attribute")
-    elif attribute in dir(a_class):
+    elif hasattr(a_class, attribute):
         raise TypeError("can't add new attribute")
     else:
         setattr(a_class, attribute, value)
