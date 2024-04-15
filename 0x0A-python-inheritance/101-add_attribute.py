bed37bd4eefb7976e8b6f2f52dcdabdcf7dd8fb7
sageXsane <sanelisiwebhengu01@gmail.com>
2024-04-16 01:05:59 +0200

    ReMake task 101

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -2,23 +2,17 @@
 """ add_attribute module"""
 
 
-def add_attribute(a_class, attribute, value):
+def add_attribute(inst, attribute, value):
     """ add_attribute method:
         adds a new attribute to an object if it’s possible
         (only to custom classes and not builtin ones)
 
         Args:
-            a_class: class name
+            inst: object instance
             attribute: atrribute
             value: attribute value
     """
-    if not isinstance(type(a_class), type):
-        raise TypeError("can't add new attribute")
-    if isinstance(a_class, (int, float, str, tuple)):
-        raise TypeError("can't add new attribute")
-    elif '__slots__' in dir(a_class) or not hasattr(a_class, "__dict__"):
-        raise TypeError("can't add new attribute")
-    elif hasattr(a_class, attribute):
+    if not hasattr(inst, "__dict__"):
         raise TypeError("can't add new attribute")
     else:
-        setattr(a_class, attribute, value)
+        setattr(inst, attribute, value)
